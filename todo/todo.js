6159a019dd98c0004df7b63521b53606498d12f4
Daria Kolodzey <kolodaria@google.com>
2015-05-27 13:22:45 +0300

    adding and deleting todos affecting model list

--- todo/todo.js
+++ todo/todo.js
@@ -1,24 +1,54 @@
-/*global $ */
+var $; /* jQuery global variable */
+var console;
+
 $(document).ready(function () {
     'use strict';
     function Todo(todo, checked) {
         this.todo = todo;
         this.checked = checked;
     }
-    var todos = [new Todo("find coworking", "checked"),
-                 new Todo("find work", "")],
-        i;
-    for (i = 0; i < todos.length; i += 1) {
+    Todo.prototype.addToDOM = function (add_method) {
         $('.todolist ul')
-            .append(
+            [add_method](
                 '<li><input type="checkbox" '
-                    + todos[i].checked
+                    + this.checked
                     + '> <p>'
-                    + todos[i].todo
+                    + this.todo
                     + '</p><form class="change-current">'
                     + '<input type="submit" value="Edit">'
                     + '<input type="submit" value="Delete">'
                     + '</form></li>'
             );
+    };
+    
+    var todos = [new Todo('find coworking', 'checked'),
+                 new Todo('find work', '')],
+        i;
+    for (i = 0; i < todos.length; i += 1) {
+        todos[i].addToDOM('append');
     }
+    console.log(JSON.stringify(todos));
+    
+    $(document).on('click', 'input[value="Delete"]', function () {
+        var cur_element = $(this).parents('li');
+        todos.splice($('li').index(cur_element), 1);
+        cur_element.fadeOut();
+        cur_element.remove();
+        event.preventDefault();
+        console.log(JSON.stringify(todos));
+    });
+    $(document).on('click', 'input[value="Edit"]', function () {
+        console.log($(this).parents('li').children('p').text());
+        event.preventDefault();
+    });
+    $(document).on('submit', '.change-current', function (event) {
+        event.preventDefault();
+    });
+    $('.add-todo').submit(function (event) {
+        var item = new Todo($('.add-todo input').val(), '');
+        todos.splice(0, 0, item);
+        item.addToDOM('prepend');
+        event.preventDefault();
+        console.log(JSON.stringify(todos));
+    });
 });
